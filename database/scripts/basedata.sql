4584c79cac5e553e27df3e0fd63c330c0aaf8543
Paulo Trentin <paulo@paulotrentin.com.br>
2017-02-12 21:35:20 -0200

    Adicionado novas tarefas para alimentar base padrão do sistema

--- database/scripts/basedata.sql
+++ database/scripts/basedata.sql
@@ -11,3 +11,16 @@ INSERT INTO `activity` (`id`, `name`, `punctuation`, `description`) VALUES
 (NULL, 'Varrer um cômodo', '2', NULL), 
 (NULL, 'Varrer a casa toda', '5', NULL), 
 (NULL, 'Passar pano em um cômodo', '2', NULL);
+(NULL, 'Passar pano na casa toda', '5', '1', NULL), 
+(NULL, 'Limpar o banheiro', '5', '1', NULL), 
+(NULL, 'Limpar box do banheiro', '3', '1', NULL), 
+(NULL, 'Colocar roupa para lavar', '2', '1', NULL), 
+(NULL, 'Estender roupa', '3', '1', NULL), 
+(NULL, 'Dobrar roupa para passar', '3', '1', NULL), 
+(NULL, 'Passar 30 minutos de roupa', '10', '1', NULL), 
+(NULL, 'Tirar pó de móveis', '5', '1', NULL), 
+(NULL, 'Limpar as janelas', '10', '1', NULL), 
+(NULL, 'Limpar os espelhos', '4', '1', NULL), 
+(NULL, 'Arrumar a cama', '1', '1', NULL), 
+(NULL, 'Limpar a geladeira', '10', '1', NULL), 
+(NULL, 'Ir no mercado', '10', '1', NULL);
